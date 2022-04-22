d483f13266c99848fe090d45f0e43f3e8f420888
Breeda Herlihy <G00411369@gmit.ie>
2022-04-22 16:12:55 +0100

    outline

--- analysis.py
+++ analysis.py
@@ -1,6 +1,27 @@
 # Program to analyse a dataset on Iris
 # Author: Breeda Herlihy
 
+# import relevant modules
+import pandas as pd
+
+# perform data analysis using (pandas) buit in statistical data
+# https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/ 
+# output summary data in text file (pandas)
+# output data in visual formats using Matplotlib / Seaborn
+    # summary of each variable
+    # histogram of each variable
+    # scatter plot of each pair of variables
+    # any other analyses
+# https://medium.com/@siddhardhan23/data-visualization-in-python-a90ddb706b23 [Pair plot example, data visualisation, univariate, multivariate analysis]
+# https://www.sisense.com/blog/data-visualizations-in-python-and-r/
+
+
+# read in data from the file and create a dataframe
+filename = 'irisdata.csv'
+df = pd.read_csv(filename)
+print(df)
+
+
 filename = "iris.data"
 def readText():
  with open(filename,"r") as f:
@@ -11,3 +32,6 @@ chapter = readText()
 
 irisCount= readText().count("Iris")
 print(irisCount)
+
+#References
+
