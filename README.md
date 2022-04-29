e4a39bddd10faada14c3463f1b3a8b77c064bfbd
Breeda Herlihy <G00411369@gmit.ie>
2022-04-29 13:17:56 +0100

    iteration

--- README.md
+++ README.md
@@ -4,17 +4,57 @@
 ## Data set
 
 ## Analysis
+# perform data analysis using (pandas) buit in statistical data
+# https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/ 
+# output summary data in text file (pandas)
+# https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
+    # summary of each variable
+
+
+
+
+
+# output data in visual formats using Matplotlib / Seaborn
+    # histogram of each variable
+    # scatter plot of each pair of variables
+    # any other analyses
+# https://medium.com/@siddhardhan23/data-visualization-in-python-a90ddb706b23 [Pair plot example, data visualisation, univariate, multivariate analysis]
+# https://www.sisense.com/blog/data-visualizations-in-python-and-r/
 
 ### Read in data
 
 ### Analyse each variable
 
 ### Output summary data to a text file
+# https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
+# https://pandas.pydata.org/docs/getting_started/intro_tutorials/06_calculate_statistics.html
+# correlation matrix https://www.geeksforgeeks.org/how-to-create-a-correlation-matrix-using-pandas/#:~:text=variables%20are%20related.-,Pandas%20dataframe.,na%20values%20are%20automatically%20excluded. 
+# https://stackoverflow.com/questions/2918362/writing-string-to-a-file-on-a-new-line-every-time
+
+# https://pandas.pydata.org/docs/user_guide/style.html# Table Visualisation - consider
+# https://pandas.pydata.org/docs/user_guide/basics.html#sorting sorting results
+# https://pandas.pydata.org/docs/user_guide/reshaping.html pivot tables
+
+
 
 ### Output histograms
+# https://realpython.com/python-histograms/
+# https://stackoverflow.com/questions/6871201/plot-two-histograms-on-single-chart-with-matplotlib
+# seaborn histogram
+# https://stackoverflow.com/questions/36362624/how-to-plot-multiple-histograms-on-same-plot-with-seaborn
+# bin size https://seaborn.pydata.org/generated/seaborn.histplot.html#seaborn.histplot
+# graph subtitle https://stackoverflow.com/questions/1388450/giving-graphs-a-subtitle-in-matplotlib 
+# save the plots # https://www.geeksforgeeks.org/matplotlib-pyplot-savefig-in-python/ 
 
 ### Output scatterplots
+# seaborn scatter plots
+# https://www.geeksforgeeks.org/scatterplot-using-seaborn-in-python/
 
+
+### Any other analyses
+Multivariate analysis of variables, pair plot of variable class (species)
+# https://seaborn.pydata.org/tutorial/axis_grids.html
+# https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/ 
 
 ## References 
 https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/
--- analysis.py
+++ analysis.py
@@ -1,42 +1,69 @@
 # Program to analyse a dataset on Iris
 # Author: Breeda Herlihy
 
-# import relevant modules
+# import relevant modules and libraries
 
 from fileinput import filename
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# perform data analysis using (pandas) buit in statistical data
-# https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/ 
-# output summary data in text file (pandas)
-# https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
-    # summary of each variable
-# output data in visual formats using Matplotlib / Seaborn
-    # histogram of each variable
-    # scatter plot of each pair of variables
-    # any other analyses
-# https://medium.com/@siddhardhan23/data-visualization-in-python-a90ddb706b23 [Pair plot example, data visualisation, univariate, multivariate analysis]
-# https://www.sisense.com/blog/data-visualizations-in-python-and-r/
-
-
-# read in data from the file and create a dataframe
-filename = 'iris.data'
+# Read in dataset and create a dataframe
+filename = 'bezdekIris.data'
 df = pd.read_csv(filename, names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"])
 
-# plot histograms
-# https://realpython.com/python-histograms/
-# https://stackoverflow.com/questions/6871201/plot-two-histograms-on-single-chart-with-matplotlib
+# Outputs a summary of each variable to a single text file
+f = open("summary.txt", "a")
+
+print("Summary data for each variable\n", file=f)
+print(df["sepal_length"].describe(), file=f)
+print(df["sepal_width"].describe(), file=f)
+print(df["petal_length"].describe(), file=f)
+print(df["petal_width"].describe(), file=f)
+
+print("Variables grouped by Iris species", file=f)
+print(df[["sepal_length","species"]].groupby("species").describe(), file=f)
+print(df[["sepal_width","species"]].groupby("species").describe(), file=f)
+print(df[["petal_length","species"]].groupby("species").describe(), file=f)
+print(df[["petal_width","species"]].groupby("species").describe(), file=f)
+
+print("\nCorrelation Matrix for Iris dataset variables", file=f)
+print(df.corr(), file=f)
+
 '''
+#a_dataframe = df.head(5)
+#numpy_array = a_dataframe.to_numpy()
+#np.savetxt("test_file.txt",numpy_array, fmt = "%s")
+#
+# top 5 rows of df
+# print(df.head(5))
+# random sample of 10 rows
+# print(df.sample(10))
+# list the column headers and data types
+# print(df.columns)
+# list the number of rows and number of columns
+# print(df.shape)
+# slice data e.g. for one species
+# print(df[100:150])
+# use loc and iloc to display specific rows
+# print(df.iloc[2])
+# return data where the species matches the str
+# print(df.loc[df['species'] == "Iris-virginica"])
+# print(df["species"].value_counts())
+
+
+
+# plot histograms
+
+
 # pandas histogram
 df['sepal_length'].hist(by=df['species'])
 df.hist(column="sepal_length", by="species", grid=True, bins=10, legend=True)
 df.hist(column="sepal_width", by="species", grid=True, bins=10, legend=True)
 df.hist(column="petal_length", by="species", grid=True, bins=10, legend=True)
 df.hist(column="petal_width", by="species", grid=True, bins=10, legend=True)
-'''
-'''
+
+
 # matplotlib histogram cannot use objects
 iris_vi = df.loc[df['species'] == "Iris-virginica"]
 iris_se = df.loc[df['species'] == "Iris-secosa"]
@@ -58,7 +85,7 @@ ax.bar_label(rects2, padding=3)
 fig.tight_layout()
 '''
 
-'''
+''''
 #seaborn histogram
 # https://stackoverflow.com/questions/36362624/how-to-plot-multiple-histograms-on-same-plot-with-seaborn
 plt.rc("grid", linestyle = "dotted", color = "gray", alpha = 0.7)
@@ -69,75 +96,25 @@ ax = sns.histplot(data = df, x = "sepal_length",
                          bins = 25,  # https://seaborn.pydata.org/generated/seaborn.histplot.html#seaborn.histplot
                          element = "bars") 
 ax.set_xlabel("Sepal length / cm", fontsize = 10)
-# https://stackoverflow.com/questions/1388450/giving-graphs-a-subtitle-in-matplotlib 
+
 plt.title("Sepal length distribution of 3 Iris species", weight = "bold")
-# https://www.geeksforgeeks.org/matplotlib-pyplot-savefig-in-python/
 plt.savefig("histogram_sepal_length.png")
 plt.show()
-'''
-#seaborn scatter plots
-# https://www.geeksforgeeks.org/scatterplot-using-seaborn-in-python/
+
+# Scatter plots
 sns.set(style='whitegrid')
 sns.scatterplot(x="sepal_length", y="sepal_width", hue="species", data=df)
 plt.title("Sepal length versus width for Iris species", weight = "bold")
 plt.xlabel("Sepal length / cm", fontsize=8)
 plt.ylabel("Sepal width / cm", fontsize=8)
 plt.savefig("scatterplot_sepal_length_v_width.png")
-#plt.show()
+plt.show()
 
 #sns.scatterplot(x="petal_length", y="petal_width", hue="species", data=df)
 #plt.savefig("scatterplot_petal_length_v_width.png")
-#plt.show()
+plt.show()
 
 '''
-# Summary of each variable 
-# https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file
-f = open("summary.txt", "a")
-
-#https://pandas.pydata.org/docs/getting_started/intro_tutorials/06_calculate_statistics.html
-print(df["sepal_length"].describe(), file=f)
-print(df["sepal_width"].describe(), file=f)
-print(df["petal_length"].describe(), file=f)
-print(df["petal_width"].describe(), file=f)
-
-print("Variables grouped by Species", file=f)
-print(df[["sepal_length","species"]].groupby("species").describe(), file=f)
-print(df[["sepal_width","species"]].groupby("species").describe(), file=f)
-print(df[["petal_length","species"]].groupby("species").describe(), file=f)
-print(df[["petal_width","species"]].groupby("species").describe(), file=f)
-https://www.geeksforgeeks.org/how-to-create-a-correlation-matrix-using-pandas/#:~:text=variables%20are%20related.-,Pandas%20dataframe.,na%20values%20are%20automatically%20excluded.
-print("Correlation Matrix for Iris dataset variables", file=f)
-print(df.corr(), file=f)
-
-f.close()
-# https://pandas.pydata.org/docs/user_guide/style.html# Table Visualisation - consider
-# https://pandas.pydata.org/docs/user_guide/basics.html#sorting sorting results
-# https://pandas.pydata.org/docs/user_guide/reshaping.html pivot tables
-
-'''
-
-
-
-#a_dataframe = df.head(5)
-#numpy_array = a_dataframe.to_numpy()
-#np.savetxt("test_file.txt",numpy_array, fmt = "%s")
-#https://www.adamsmith.haus/python/answers/how-to-write-contents-of-a-dataframe-into-a-text-file-in-python
-# top 5 rows of df
-# print(df.head(5))
-# random sample of 10 rows
-# print(df.sample(10))
-# list the column headers and data types
-# print(df.columns)
-# list the number of rows and number of columns
-# print(df.shape)
-# slice data e.g. for one species
-# print(df[100:150])
-# use loc and iloc to display specific rows
-# print(df.iloc[2])
-# return data where the species matches the str
-# print(df.loc[df['species'] == "Iris-virginica"])
-# print(df["species"].value_counts())
-
 
 
 
@@ -153,7 +130,5 @@ sns.heatmap(iris.corr(),cmap = "YlGnBu", linecolor = 'white', linewidths = 1)
 g = sns.pairplot(df,hue="class")
 '''
 
-#plt.show()
 
-#References
-# https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/
+
